de96a2b3bdd0920fe4d2788de0580aff198e6a93
Thiago Silva <thyagosousasilva@gmail.com>
2021-09-14 22:06:12 -0300

    Adding firebase core and auth to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:filmes_app/modules/splash/splash_module.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  // Inicializa o Flutter antes mesmo de rodar a aplicação,
+  // garantindo que não haverá problema ao inicializar o
+  // firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
